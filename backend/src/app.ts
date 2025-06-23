45552a5d5af1f14f7d84608c3afdd4db9fac68fa
Mukul Pal <mukulpal0333@gmail.com>
2025-06-23 16:56:51 +0530

    feat: intialize empty node js project

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Express } from "express";
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
