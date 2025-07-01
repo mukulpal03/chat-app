c0e92df6dd9e720a65fce48779fa436342a54f2f
Mukul Pal <mukulpal0333@gmail.com>
2025-07-01 12:15:58 +0530

    feat: connect to db and add user model

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import connectToDB from "./lib/db.js";
 
 const app = express();
 
@@ -7,6 +9,6 @@ const PORT = process.env.PORT ?? 4000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+connectToDB().then(() =>
+  app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
+);
