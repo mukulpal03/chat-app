68b808481543b8ae0a7c8cb47ed864aa4a24ca8b
Mukul Pal <mukulpal0333@gmail.com>
2025-07-03 16:57:24 +0530

    feat: add logout functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Navbar from "./components/Navbar";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -46,6 +47,8 @@ const App = () => {
           element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />}
         />
       </Routes>
+
+      <Toaster />
     </div>
   );
 };
