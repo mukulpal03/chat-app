b41b72cd91eafc6dde1be4d73461957e94fa410a
Mukul Pal <mukulpal0333@gmail.com>
2025-07-03 21:07:32 +0530

    feat: add settings page for multiple themes

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,8 @@ const app = express();
 
 const PORT = process.env.PORT ?? 4000;
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
