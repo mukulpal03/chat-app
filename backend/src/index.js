f0b1630ec06c3b8b0eff47639c48e163079659af
Mukul Pal <mukulpal0333@gmail.com>
2025-07-04 23:05:12 +0530

    edit

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,8 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import { app, server } from "./lib/socket.js";
-import path from "path";
 
 const PORT = process.env.PORT ?? 4001;
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -23,17 +21,6 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  console.log("NODE_ENV:", process.env.NODE_ENV);
-  console.log("__dirname:", __dirname);
-  console.log("Static path:", path.join(__dirname, "../frontend/dist"));
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
 
 connectToDB().then(() =>
   server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
