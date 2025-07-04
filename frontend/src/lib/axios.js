3061466d8dd4b678a2d4511b227d86049b6acad1
Mukul Pal <mukulpal0333@gmail.com>
2025-07-04 21:58:46 +0530

    feat: setup for prod

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:4000/api"
+      : "/api",
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,6 +3,9 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSignInUp: false,
@@ -85,7 +88,7 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
 
-    const socket = io("http://localhost:4000", {
+    const socket = io(BASE_URL, {
       query: {
         userId: authUser._id,
       },
